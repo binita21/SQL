1d3af4651ef79ae61608034bff21f1c50e4865b3
Binita Sapkota <binitasapkota012@gmail.com>
2026-09-03 21:29:25 +0545

    select statement

---
+++ 1. Select statement.sql
@@ -0,0 +1,12 @@
+SELECT first_name,last_name,birth_date,age,(age+10)*10+10
+FROM parks_and_recreation.employee_demographics;
+
+
+SELECT DISTINCT first_name
+FROM parks_and_recreation.employee_demographics;
+
+
+ SELECT DISTINCT first_name,gender
+ FROM parks_and_recreation.employee_demographics;
+
+ 
