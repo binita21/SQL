b75560ec3594b74f9f79c9d6881a7d543fbf8441
Binita Sapkota <binitasapkota012@gmail.com>
2026-09-16 21:25:12 +0545

    updated practice

--- practice.sql
+++ practice.sql
@@ -66,3 +66,43 @@ WHERE (country LIKE '%A'  OR
    OR country LIKE 'I%'
    OR country LIKE 'O%'
    OR country LIKE 'U%');
+   
+   -- Select list of country and should not start with vowels and should be distinct.
+SELECT DISTINCT country
+FROM layoffs_staging2
+WHERE LEFT(country,1) NOT IN ('a','e','i','o','u');
+
+  -- Select list of country and should not end with vowels and should be distinct.
+SELECT DISTINCT country
+FROM layoffs_staging2
+WHERE RIGHT(country,1) NOT IN ('a','e','i','o','u');
+
+-- Query the list of Country names from table that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT country
+FROM layoffs_staging2
+WHERE LEFT(country,1) NOT IN ('a','e','i','o','u')
+OR RIGHT(country,1) NOT IN ('a','e','i','o','u')
+;
+
+-- Query the list of Country names from layoffs_staging2 that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT country
+FROM layoffs_staging2
+WHERE LEFT(country,1) NOT IN ('a','e','i','o','u')
+AND RIGHT(country,1) NOT IN ('a','e','i','o','u');
+;
+
+-- Query the Name of any company in layoffs_staging2 whose fund_raised is higher than  200. Order your output by the last three characters of each name of company. 
+-- If two or more company both have names ending in the same last three characters , secondary sort them by ascending date.
+SELECT * FROM layoffs_staging2;
+SELECT company
+FROM layoffs_staging2
+WHERE funds_raised_millions > 200
+ORDER BY RIGHT(company,3) ,`date` ASC;
+
+-- Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+use parks_and_recreation;
+
+SELECT first_name
+FROM employee_demographics
+ORDER BY first_name ASC;
+
