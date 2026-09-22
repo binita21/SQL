f9d0a1dc30fd3acbf0f1ce839bff7baa5d31adee
Binita Sapkota <binitasapkota012@gmail.com>
2026-09-22 21:46:18 +0545

    updated data cleaning

--- data_cleaning_sql.sql
+++ data_cleaning_sql.sql
@@ -193,6 +193,15 @@ where company like "Bally's%";
  ALTER TABLE layoffs_staging2
  DROP column row_num;
  
- 
+ /*SELECT
+    CASE
+        WHEN A + B <= C OR A + C <= B OR B + C <= A
+            THEN 'Not A Triangle'
+        WHEN A = B AND B = C
+            THEN 'Equilateral'
+        WHEN A = B OR B = C OR A = C
+            THEN 'Isosceles'
+        ELSE 'Scalene'
+    END
+FROM TRIANGLES;*/
 
- 
