c2aac0819af4897e50455290051fdcb1269adcd9
Binita Sapkota <binitasapkota012@gmail.com>
2026-09-23 22:10:42 +0545

    Updated student_score.sql

--- student_score.sql
+++ student_score.sql
@@ -11,6 +11,14 @@ SELECT *
 FROM scores
 WHERE `Student Placed` = 'Yes'; -- Student who got placed.
 
+
+
+-- Total number of students
+SELECT COUNT(*) 
+AS total_records
+FROM scores;
+
+
 -- What is the average score in SQL?
 SELECT round( AVG(`Sql`),2 )as avg_score_in_sql
 FROM scores;
@@ -27,25 +35,73 @@ FROM scores;
 SELECT round( AVG(`Tableau`),2 )as avg_score_in_Tableau
 FROM scores;
 
+
+-- 
 WITH avg_score AS
 (
-SELECT AVG(`Sql`),AVG(`Python`),AVG(`ML`),AVG(`Tableau`),AVG(`Excel`)
+SELECT round(AVG(`Sql`),2) as sql_avg_score,
+round(AVG(`Python`),2) as python_avg_score,
+round(AVG(`ML`),2) Ml_avg_score,
+ round(AVG(`Tableau`),2) Tableau_avg_score,
+ round(AVG(`Excel`),2) Excel_avg_score
 FROM scores
 )
 SELECT * FROM avg_score;
 
-WITH high_avg_score as(
-SELECT GREATEST(AVG(`Sql`),AVG(`Python`),AVG(`ML`),AVG(`Tableau`),AVG(`Excel`)) as highest_avg_score
-FROM scores)
-SELECT * FROM
-high_avg_score ;
-
-
-SELECT LEAST(
+-- Highest average score
+SELECT round(GREATEST(
    AVG(`Sql`),AVG(`Python`),AVG(`ML`),AVG(`Tableau`),AVG(`Excel`)
-) AS lowest_average
+),2)AS lowest_average
 FROM scores;
 
+-- Lowest average score
+SELECT round(LEAST(
+   AVG(`Sql`),AVG(`Python`),AVG(`ML`),AVG(`Tableau`),AVG(`Excel`)
+),2)AS lowest_average
+FROM scores;
+
+SELECT * FROM scores;
+
+-- MAX score in each subject
+SELECT 
+MAX(`Sql`) sql_max,MAX(`Python`) python_max,MAX(`ML`) Ml_max,MAX(`Tableau`) Tableau_max,MAX(`Excel`) Excel_max
+FROM scores;
+
+-- MAX score in each subject
+SELECT 
+MIN(`Sql`) sql_min,MIN(`Python`) python_min,MIN(`ML`) Ml_min,MIN(`Tableau`) Tableau_min,MIN(`Excel`) Excel_min
+FROM scores;
+
+-- What is the average score of placed students?
+WITH avg_scores as (
+SELECT 
+round(AVG(`Sql`),2) avg_sql,round(AVG(`Python`),2) avg_python,round(AVG(`ML`),2) avg_ml,round(AVG(`Tableau`),2) avg_tableau,round(AVG(`Excel`),2) avg_excel
+FROM scores
+WHERE `Student Placed` = 'Yes')
+SELECT * from avg_scores ;
 
 
+-- What is the average score of unplaced students?
+WITH avg_scores as (
+SELECT 
+round(AVG(`Sql`),2) avg_sql,round(AVG(`Python`),2) avg_python,round(AVG(`ML`),2) avg_ml,round(AVG(`Tableau`),2) avg_tableau,round(AVG(`Excel`),2) avg_excel
+FROM scores
+WHERE `Student Placed` = 'No')
+SELECT * from avg_scores ;
 
+-- What is the average score by placement status?
+SELECT `Student Placed`,round(AVG(`Sql`),2) avg_sql,round(AVG(`Python`),2) avg_python,round(AVG(`ML`),2) avg_ml,round(AVG(`Tableau`),2) avg_tableau,round(AVG(`Excel`),2) avg_excel
+FROM scores
+GROUP BY `Student Placed`;
+
+-- Number of unplaced students
+SELECT COUNT(*) 
+AS `Unplaced_student` 
+FROM scores
+WHERE `Student Placed`='No';
+
+-- Number of unplaced students
+SELECT COUNT(*)
+AS `Placed_student`
+FROM scores
+WHERE `Student Placed`='Yes';
